43d4775c34bf6f1bc671253d6526fce0476e51ce
lucorus <106882669+lucorus@users.noreply.github.com>
2023-05-11 16:40:48 +0400

    fix search

--- miniDOCS/main/views.py
+++ miniDOCS/main/views.py
@@ -1,5 +1,3 @@
-from django.core.paginator import Paginator
-from django.db.models import Q
 from django.shortcuts import render
 from .models import *
 
@@ -10,15 +8,15 @@ def main_page(request):
 
 def search(request):
     search_query = request.GET.get('q')
+    search_query = search_query.strip()
+    search_query = search_query.replace(' ', 'a')
     if search_query:
         tag = Tag.objects.filter(title__icontains=search_query)
     else:
         tag = Tag.objects.get(pk=0)
-    #func = Func.objects.get(tag__title=tag)
     return render(request, 'language/tag.html', {'tag': tag, 'value': search_query, 'language': Language.objects.all()})
 
 
 def language(request, language):
     func = Func.objects.filter(language__title=language)
-    #func = search(request)
     return render(request, 'language/language.html', {'func': func, 'language': Language.objects.all(), 'lang': language})
